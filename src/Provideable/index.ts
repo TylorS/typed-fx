00f479dea39f41bc8c520886a3b084e0539a41f1
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-08-14 21:47:46 -0400

    docs: add mention of how to use Provideable

--- src/Provideable/index.ts
+++ src/Provideable/index.ts
@@ -4,6 +4,40 @@ import type { Fx, IO } from '@/Fx/Fx.js'
 export const PROVIDEABLE = Symbol('PROVIDEABLE')
 export type PROVIDEABLE = typeof PROVIDEABLE
 
+/**
+ * Provideable is an interface Services can implement to help you create an Env or provide it to an Fx.
+ * Generally meant to be used to help create synchronous Envs. Service.Id implements this interface and is
+ * the easiest way to utilize this.
+ *
+ * @example
+ * import * as S from '@typed/fx/Service'
+ * import * as Fx from '@typed/fx/Fx'
+ *
+ * export class MyService extends S.Id {
+ *   constructor(readonly foo: string) {}
+ * }
+ *
+ * const program = Fx.Fx(function*() {
+ *  const { foo } = yield* MyService.ask()
+ *  ...
+ * })
+ *
+ * // With Fx.provide
+ * pipe( program, Fx.provide( new MyService('example') ), Fx.runMain ).catch(error => {
+ *   console.error(error)
+ * })
+ *
+ * // Built-in provide
+ * Fx.runMain( new MyService('example').provide( program ) ).catch(error => {
+ *   console.error(error)
+ * })
+ *
+ *
+ * // With other services
+ * pipe( program, Fx.provide( new MyService('example').add( new MyOtherService() ) ), Fx.runMain ).catch(error => {
+ *   console.error(error)
+ * })
+ */
 export interface Provideable<R> {
   readonly [PROVIDEABLE]: () => Env<R>
   readonly provide: <E, A>(fx: Fx<R, E, A>) => IO<E, A>
