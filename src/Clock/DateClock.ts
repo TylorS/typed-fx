3ed4ecc2d59689412165e3443077e5738df92785
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-08-14 21:34:36 -0400

    fix: doesn't implement Provideable

--- src/Clock/DateClock.ts
+++ src/Clock/DateClock.ts
@@ -1,9 +1,8 @@
 import { Clock, make } from './Clock.js'
 
-import { Provideable } from '@/Provideable/index.js'
 import { Time, UnixTime } from '@/Time/index.js'
 
-export function DateClock(startTime: Date = new Date()): Clock & Provideable<Clock> {
+export function DateClock(startTime: Date = new Date()): Clock {
   const offset = startTime.getTime()
 
   return make(UnixTime(offset), () => Time(new Date().getTime() - offset))
