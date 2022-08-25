361ea685028391fcc1ea130ea1c2f003280150ae
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-08-24 21:33:24 -0400

    refactor: delete Eff abstraction

--- src/Eff/Instructions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Failure.js'
-export * from './FromLazy.js'
-export * from './Trace.js'
--- src/Eff/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Eff.js'
-export * from './hkt.js'
-export * from './Instructions/index.js'
