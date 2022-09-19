e48d01f39ae9cb63066fe4e97fb655acfbf68866
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-09-19 12:19:53 -0400

    refactor: cleanup Stream tests

--- src/Stream/flatMap.test.ts
+++ src/Stream/flatMap.test.ts
@@ -2,13 +2,11 @@ import { deepStrictEqual } from 'assert'
 
 import { pipe } from 'hkt-ts'
 
-import { collect, observe } from './drain.js'
+import { collectAll } from './_internal.test.js'
 import { flatMap } from './flatMap.js'
 import { fromFx } from './fromFx.js'
 
 import * as Fx from '@/Fx/index.js'
-import { RootScheduler } from '@/Scheduler/RootScheduler.js'
-import { Scheduler } from '@/Scheduler/Scheduler.js'
 import { testSuite } from '@/_internal/suite.js'
 
 testSuite(import.meta.url, () => {
@@ -19,25 +17,9 @@ testSuite(import.meta.url, () => {
       flatMap((a) => fromFx(Fx.success(a + 1))),
     )
 
-    it('should create a Stream', () =>
-      pipe(
-        stream,
-        observe((n) => Fx.fromLazy(() => deepStrictEqual(n, value + 1))),
-        Fx.flatMap(Fx.join),
-        Fx.provideService(Scheduler, RootScheduler()),
-        Fx.runMain,
-      ))
-
     it('should be collectable', async () => {
-      await pipe(
-        Fx.Fx(function* () {
-          const events: readonly number[] = yield* collect(stream)
-
-          deepStrictEqual(events, [value + 1])
-        }),
-        Fx.provideService(Scheduler, RootScheduler()),
-        Fx.runMain,
-      )
+      const events = await collectAll(stream)
+      deepStrictEqual(events, [value + 1])
     })
   })
 })
