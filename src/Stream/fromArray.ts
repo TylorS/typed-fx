9d2ba8a5cc338f0231f053f681a688424c53c908
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-09-23 23:37:43 -0400

    feat: allow fromArray to fail

--- src/Stream/fromArray.ts
+++ src/Stream/fromArray.ts
@@ -1,8 +1,29 @@
+import { pipe } from 'hkt-ts'
+import * as NEA from 'hkt-ts/NonEmptyArray'
+
 import { fromCallback } from './fromCallback.js'
 
+import { makeParallelIdentity, unexpected } from '@/Cause/Cause.js'
+
+const foldCauses = NEA.foldLeft(makeParallelIdentity<never>())
+
 export function fromArray<A>(array: ReadonlyArray<A>) {
-  return fromCallback<never, A>(async ({ event, end }) => {
-    await Promise.all(array.map(event))
-    await end()
+  return fromCallback<never, A>(async ({ event, error, end }) => {
+    const results = await Promise.allSettled(array.map(event))
+    const failedResults = results.filter(
+      (result): result is PromiseRejectedResult => result.status === 'rejected',
+    )
+
+    if (NEA.isNonEmpty(failedResults)) {
+      const cause = pipe(
+        failedResults,
+        NEA.map((result) => unexpected(result.reason)),
+        foldCauses,
+      )
+
+      await error(cause)
+    } else {
+      await end()
+    }
   })
 }
