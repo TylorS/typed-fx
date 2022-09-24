616a4049e9cc0254195ab90ceec074d7fc6395e7
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-09-24 14:15:26 -0400

    feat(Stream): zipItems
    Co-authored-by: andrew jarrett <ahrjarrett@gmail.com>

---
+++ src/Stream/zipItems.ts
@@ -0,0 +1,41 @@
+import { pipe, second } from 'hkt-ts/function'
+
+import { Stream } from './Stream.js'
+
+import { Sink } from '@/Sink/Sink.js'
+
+export function zipItems<A, B, C>(f: (a: A, b: B) => C, array: ReadonlyArray<B>) {
+  return <R, E>(stream: Stream<R, E, A>): Stream<R, E, C> => {
+    return Stream((sink, scheduler, context) =>
+      stream.fork(new ZipItemSink(sink, f, array), scheduler, context),
+    )
+  }
+}
+
+export function withItems<B>(array: ReadonlyArray<B>) {
+  return <R, E, A>(stream: Stream<R, E, A>): Stream<R, E, B> =>
+    pipe(stream, zipItems(second, array))
+}
+
+class ZipItemSink<A, B, C, E, E2> {
+  protected _index = 0
+  protected _endIndex = this.array.length - 1
+
+  constructor(
+    readonly sink: Sink<E, C, E2>,
+    readonly f: (a: A, b: B) => C,
+    readonly array: ReadonlyArray<B>,
+  ) {}
+
+  event(a: A) {
+    if (this._index <= this._endIndex) {
+      this._index += 1
+      return this.sink.event(this.f(a, this.array[this._index]))
+    }
+
+    return this.sink.end
+  }
+
+  error = this.sink.error
+  end = this.sink.end
+}
