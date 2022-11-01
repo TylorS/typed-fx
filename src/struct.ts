76ff768e17196c95a56a925b99551502f9a5fb1a
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-10-31 21:33:43 -0400

    feat: struct

---
+++ src/struct.ts
@@ -0,0 +1,24 @@
+import { pipe } from '@fp-ts/data/Function'
+
+import { Fx } from './Fx.js'
+import { combineAll } from './combine.js'
+import { map } from './filterMap.js'
+
+export const struct = <Props extends Readonly<Record<string, Fx<any, any, any>>>>(
+  props: Props,
+): Fx<
+  Fx.ResourcesOf<Props[string]>,
+  Fx.ErrorsOf<Props[string]>,
+  { readonly [K in keyof Props]: Fx.OutputOf<Props[K]> }
+> =>
+  pipe(
+    combineAll(
+      Object.entries(props).map(([key, fx]) =>
+        pipe(
+          fx,
+          map((value) => ({ [key]: value })),
+        ),
+      ),
+    ),
+    map((values) => Object.assign({}, ...values)),
+  )
