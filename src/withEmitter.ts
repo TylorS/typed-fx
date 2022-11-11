da3a682f30a41b4c7c0f95faffa15ab1c05ee391
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-11-10 22:23:50 -0500

    fix: use a runtime and deferred to ensure functions

--- src/withEmitter.ts
+++ src/withEmitter.ts
@@ -1,9 +1,7 @@
+import * as Deferred from '@effect/core/io/Deferred'
 import * as Effect from '@effect/core/io/Effect'
 import * as Exit from '@effect/core/io/Exit'
-import { unsafeForkUnstarted } from '@effect/core/io/Fiber/_internal/runtime'
-import { Scope } from '@effect/core/io/Scope'
-import { flow, pipe } from '@fp-ts/data/Function'
-import { left } from '@tsplus/stdlib/data/Either'
+import { pipe } from '@tsplus/stdlib/data/Function'
 
 import { Emitter, Fx } from './Fx.js'
 import type { UnsafeEmitter } from './Subject.js'
@@ -11,36 +9,31 @@ import type { UnsafeEmitter } from './Subject.js'
 export function withEmitter<R, E, A>(
   f: (emitter: UnsafeEmitter<E, A>) => Effect.Canceler<R>,
 ): Fx<R, E, A> {
-  return Fx<R, E, A>(<R2>(sink: Emitter<R2, E, A>) => {
-    return Effect.withFiberRuntime<R | R2 | Scope, never, unknown>((fiber, status) => {
-      const unsafeRun = <R, E, A>(
-        eff: Effect.Effect<R, E, A>,
-        observer: (exit: Exit.Exit<E, A>) => void,
-      ) => {
-        const f = unsafeForkUnstarted(eff, fiber, status.runtimeFlags)
-        f.addObserver(observer)
-        f.start(eff)
-      }
+  return Fx<R, E, A>(<R2>(sink: Emitter<R2, E, A>) =>
+    Effect.gen(function* ($) {
+      const runtime = yield* $(Effect.runtime<R | R2>())
+      const deferred = yield* $(Deferred.make<never, unknown>())
 
       let canceler: Effect.Canceler<R> = Effect.unit
 
-      return pipe(
-        Effect.addFinalizer(Effect.suspendSucceed(() => canceler)),
-        Effect.zipRight(
-          Effect.asyncInterrupt<R | R2, never, unknown>((cb) => {
-            canceler = f({
-              unsafeEmit: (a) =>
-                unsafeRun(sink.emit(a), (exit) =>
-                  Exit.isFailure(exit) ? cb(Effect.failCause(exit.cause)) : undefined,
-                ),
-              unsafeFailCause: (e) => unsafeRun(sink.failCause(e), flow(Effect.done, cb)),
-              unsafeEnd: () => unsafeRun(sink.end, flow(Effect.done, cb)),
-            })
+      yield* $(Effect.addFinalizer(Effect.suspendSucceed(() => canceler)))
 
-            return left(canceler)
-          }),
-        ),
-      )
-    })
-  })
+      canceler = f({
+        unsafeEmit: (a) =>
+          runtime.unsafeRunAsyncWith(sink.emit(a), (exit) =>
+            Exit.isFailure(exit) ? deferred.unsafeDone(Effect.failCause(exit.cause)) : undefined,
+          ),
+        unsafeFailCause: (e) =>
+          runtime.unsafeRunAsyncWith(sink.failCause(e), (exit) =>
+            pipe(exit, Effect.done, deferred.unsafeDone),
+          ),
+        unsafeEnd: () =>
+          runtime.unsafeRunAsyncWith(sink.end, (exit) =>
+            pipe(exit, Effect.done, deferred.unsafeDone),
+          ),
+      })
+
+      yield* $(Effect.onInterrupt(() => canceler)(deferred.await))
+    }),
+  )
 }
