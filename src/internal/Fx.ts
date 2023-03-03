f9db5625806b0f12f4709fd552378c52da7dceca
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-03-03 00:16:40 -0500

    fix: remove tag function for now

--- src/internal/Fx.ts
+++ src/internal/Fx.ts
@@ -1,14 +1,10 @@
 import type * as fx from "@typed/fx/Fx"
 
 export function isFx(value: unknown): value is fx.Fx<unknown, unknown, unknown> {
-  return isObject(value) && isTagged(value) && hasRunFunction(value)
+  return isObject(value) && hasRunFunction(value)
 }
 
 const isObject = (value: unknown): value is object => value !== null && typeof value === "object"
 
-const isTagged = (value: object): value is Tagged => "_tag" in value && typeof value["_tag"] === "string"
-
-type Tagged = { readonly _tag: string }
-
-const hasRunFunction = (value: Tagged): value is fx.Fx<unknown, unknown, unknown> =>
-  "run" in value && typeof value["run"] === "function"
+const hasRunFunction = (value: object): value is fx.Fx<unknown, unknown, unknown> =>
+  "run" in value && typeof value["run"] === "function" && value["run"].length === 1
