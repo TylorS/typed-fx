c112b3d9196d8560978a912c53941da93b9fad9b
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-03-16 22:03:41 -0400

    feat: add uninterruptable effect in fromFxEffect
    Ensures that the effect can reliably add resources to the Scope for
    finalization to occur.

--- src/internal/constructor/gen.ts
+++ src/internal/constructor/gen.ts
@@ -7,18 +7,32 @@ import { BaseFx } from "@typed/fx/internal/Fx"
 
 export const gen: <Eff extends Effect.EffectGen<any, any, any>, R, E, A>(
   f: (resume: EffectResume) => Generator<Eff, Fx<R, E, A>, unknown>
-) => Fx<R | Exclude<GenResources<Eff>, Scope.Scope>, E | GenErrors<Eff>, A> = methodWithTrace((trace) =>
-  <Eff extends Effect.EffectGen<any, any, any>, R, E, A>(
-    f: (resume: EffectResume) => Generator<Eff, Fx<R, E, A>>
-  ): Fx<R | Exclude<GenResources<Eff>, Scope.Scope>, E | GenErrors<Eff>, A> => new GenFx(f).traced(trace)
+) => Fx<R | Exclude<GenResources<Eff>, Scope.Scope>, E | GenErrors<Eff>, A> = methodWithTrace(
+  (trace) =>
+    <Eff extends Effect.EffectGen<any, any, any>, R, E, A>(
+      f: (resume: EffectResume) => Generator<Eff, Fx<R, E, A>>
+    ): Fx<
+      R | Exclude<GenResources<Eff>, Scope.Scope>,
+      E | GenErrors<Eff>,
+      A
+    > => new GenFx(f).traced(trace)
 )
 
-export class GenFx<Eff extends Effect.EffectGen<any, any, any>, R, E, A>
-  extends BaseFx<R | Exclude<GenResources<Eff>, Scope.Scope>, E | GenErrors<Eff>, A>
-{
+export class GenFx<
+  Eff extends Effect.EffectGen<any, any, any>,
+  R,
+  E,
+  A
+> extends BaseFx<
+  R | Exclude<GenResources<Eff>, Scope.Scope>,
+  E | GenErrors<Eff>,
+  A
+> {
   readonly _tag = "Gen" as const
 
-  constructor(readonly f: (resume: EffectResume) => Generator<Eff, Fx<R, E, A>>) {
+  constructor(
+    readonly f: (resume: EffectResume) => Generator<Eff, Fx<R, E, A>>
+  ) {
     super()
   }
 
@@ -26,12 +40,18 @@ export class GenFx<Eff extends Effect.EffectGen<any, any, any>, R, E, A>
    * @macro traced
    */
   run(sink: Sink<E | GenErrors<Eff>, A>) {
-    // Type-cast necessary to exclude Scope from the resources in the generator as it's always returd via run()
-    return fromFxEffect<Exclude<GenResources<Eff>, Scope.Scope>, GenErrors<Eff>, R, E, A>(Effect.gen(this.f) as any)
-      .run(sink)
+    return fromFxEffect<GenResources<Eff>, GenErrors<Eff>, R, E, A>(
+      Effect.gen(this.f)
+    ).run(sink)
   }
 }
 
-type EffectResume = <R, E, A>(effect: Effect.Effect<R, E, A>) => Effect.EffectGen<R, E, A>
-type GenResources<Eff> = [Eff] extends [never] ? never : [Eff] extends [Effect.EffectGen<infer R, any, any>] ? R : never
-type GenErrors<Eff> = [Eff] extends [never] ? never : [Eff] extends [Effect.EffectGen<any, infer E, any>] ? E : never
+type EffectResume = <R, E, A>(
+  effect: Effect.Effect<R, E, A>
+) => Effect.EffectGen<R, E, A>
+type GenResources<Eff> = [Eff] extends [never] ? never
+  : [Eff] extends [Effect.EffectGen<infer R, any, any>] ? R
+  : never
+type GenErrors<Eff> = [Eff] extends [never] ? never
+  : [Eff] extends [Effect.EffectGen<any, infer E, any>] ? E
+  : never
--- src/internal/conversion/fromFxEffect.ts
+++ src/internal/conversion/fromFxEffect.ts
@@ -1,16 +1,17 @@
 import { methodWithTrace } from "@effect/io/Debug"
-import { type Effect, matchCauseEffect } from "@effect/io/Effect"
+import { type Effect, matchCauseEffect, uninterruptible } from "@effect/io/Effect"
 import type { Fx, Sink } from "@typed/fx/Fx"
+import type { Scope } from "@typed/fx/internal/_externals"
 import { BaseFx } from "@typed/fx/internal/Fx"
 
 export const fromFxEffect: <R, E, R2 = never, E2 = never, A = unknown>(
   effect: Effect<R, E, Fx<R2, E2, A>>
-) => Fx<R | R2, E | E2, A> = methodWithTrace((trace) =>
+) => Fx<Exclude<R, Scope.Scope> | R2, E | E2, A> = methodWithTrace((trace) =>
   <R, E, R2 = never, E2 = never, A = unknown>(effect: Effect<R, E, Fx<R2, E2, A>>) =>
     new FromFxEffect(effect).traced(trace)
 )
 
-export class FromFxEffect<R, E, R2, E2, A> extends BaseFx<R | R2, E | E2, A> {
+export class FromFxEffect<R, E, R2, E2, A> extends BaseFx<Exclude<R, Scope.Scope> | R2, E | E2, A> {
   readonly _tag = "FromFxEffect" as const
 
   constructor(readonly effect: Effect<R, E, Fx<R2, E2, A>>) {
@@ -21,6 +22,7 @@ export class FromFxEffect<R, E, R2, E2, A> extends BaseFx<R | R2, E | E2, A> {
    * @macro traced
    */
   run(sink: Sink<E | E2, A>) {
-    return matchCauseEffect(this.effect, sink.error, (fx) => fx.run(sink))
+    // Ensure this.effect is uninterruptible, to ensure it can add any necessary resources to the Scope
+    return matchCauseEffect(uninterruptible(this.effect), sink.error, (fx) => fx.run(sink)) as any
   }
 }
