aef9c48dc3b5a348f805d9c83c4dec14d3c4854f
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-03-27 23:18:23 -0400

    feat: start of metrics

---
+++ src/internal/metric/tagged.ts
@@ -0,0 +1,12 @@
+import { dualWithTrace } from "@effect/io/Debug"
+import { Effect } from "@typed/fx/internal/_externals"
+import type { Fx } from "@typed/fx/internal/Fx"
+
+export const tagged: {
+  (key: string, value: string): <R, E, A>(self: Fx<R, E, A>) => Fx<R, E, A>
+  <R, E, A>(self: Fx<R, E, A>, key: string, value: string): Fx<R, E, A>
+} = dualWithTrace(
+  3,
+  (trace) =>
+    <R, E, A>(self: Fx<R, E, A>, key: string, value: string) => self.transform(Effect.tagged(key, value)).traced(trace)
+)
