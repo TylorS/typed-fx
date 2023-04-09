ad60b6a5d61c94c25fe0229b92d134f6ef3ff076
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-04-09 10:08:50 -0400

    feat: onInterrupt, promise variants

---
+++ src/onInterrupt.ts
@@ -0,0 +1,13 @@
+import type { HashSet } from "@effect/data/HashSet"
+import type { FiberId } from "@effect/io/Fiber/Id"
+import { Effect } from "@typed/fx/externals"
+import { Fx } from "@typed/fx/Fx"
+
+export function onInterrupt<R, E, A, R2, E2, B>(
+  fx: Fx<R, E, A>,
+  f: (interruptors: HashSet<FiberId>) => Effect.Effect<R2, E2, B>
+): Fx<R | R2, E | E2, A> {
+  return Fx((sink) =>
+    Effect.onInterrupt(fx.run(sink), (interruptors) => Effect.catchAllCause(f(interruptors), sink.error))
+  )
+}
