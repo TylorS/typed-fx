26a82a2cef4f70fbed39b1f12910efb36caa5bbc
Tylor Steinberger <tlsteinberger167@gmail.com>
2023-04-09 10:39:21 -0400

    feat: reduce, provide

---
+++ src/reduce.ts
@@ -0,0 +1,15 @@
+import { Effect } from "@typed/fx/externals"
+import { Fx, Sink } from "@typed/fx/Fx"
+
+export function reduce<R, E, A, B>(fx: Fx<R, E, A>, b: B, f: (b: B, a: A) => B): Fx<R, E, B> {
+  return Fx((sink) =>
+    Effect.suspend(() => {
+      let acc = b
+
+      return fx.run(Sink(
+        (a) => Effect.sync(() => acc = f(acc, a)),
+        sink.error
+      ))
+    })
+  )
+}
